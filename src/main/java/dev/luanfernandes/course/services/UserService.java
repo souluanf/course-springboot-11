af9cc8d4f28447c0f1038f4e185963aad531b978
Luan Fernandes <hello@luanfernandes.dev>
2021-02-26 18:37:51 -0300

    Camada de serviço, registro de componentes

---
+++ src/main/java/dev/luanfernandes/course/services/UserService.java
@@ -0,0 +1,26 @@
+package dev.luanfernandes.course.services;
+
+import dev.luanfernandes.course.entities.User;
+import dev.luanfernandes.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
