f7f6b869fc3b830c10707755767f30df2e5564ed
Luan Fernandes <hello@luanfernandes.dev>
2021-03-02 11:51:33 -0300

    Entidade Category

--- src/main/java/dev/luanfernandes/course/config/TestConfig.java
+++ src/main/java/dev/luanfernandes/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package dev.luanfernandes.course.config;
 
+import dev.luanfernandes.course.entities.Category;
 import dev.luanfernandes.course.entities.Order;
 import dev.luanfernandes.course.entities.User;
 import dev.luanfernandes.course.entities.enums.OrderStatus;
+import dev.luanfernandes.course.repositories.CategoryRepository;
 import dev.luanfernandes.course.repositories.OrderRepository;
 import dev.luanfernandes.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,14 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
 
     @Override
     public void run(String... args) throws Exception {
+
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -34,9 +41,14 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
     }
 
 
---
+++ src/main/java/dev/luanfernandes/course/entities/Category.java
@@ -0,0 +1,41 @@
+package dev.luanfernandes.course.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
