c705eac3f9e8a08b993c1b362a61f40cad9087de
Luan Fernandes <hello@luanfernandes.dev>
2021-03-02 12:52:49 -0300

    Entidade Category

---
+++ src/main/java/dev/luanfernandes/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package dev.luanfernandes.course.services;
+
+import dev.luanfernandes.course.entities.Category;
+import dev.luanfernandes.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
